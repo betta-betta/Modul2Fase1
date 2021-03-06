2f5ef8e29d4daa55c6622f154a4b0a02d45f205e
Lluis Gutierrez <betta_betta@hotmail.com>
2021-03-06 16:14:33 +0100

    Modul2Fase1

--- src/Modul2Fase1.java
+++ src/Modul2Fase1.java
@@ -5,15 +5,19 @@ public class Modul2Fase1 {
 public static void main(String[] args) {
 		
 
-		ArrayList<Character> nom = new ArrayList <Character>();
-		nom.add('L');
-		nom.add('l');
-		nom.add('u');
-		nom.add('i');
-		nom.add('s');
+		Character[] nom = new  Character[5];
+		nom [0]=('L');
+		nom [1]=('l');
+		nom [2]=('u');
+		nom [3]=('i');
+		nom [4]=('s');
 		
-		System.out.println("Total de caràcters al ArrayList son : "+nom.size());
-		System.out.println(nom);
+		System.out.println("Total de posicions al ArrayList son : "+nom.length);
+		System.out.println("");
+		for(int i=0;i<nom.length;i++) {
+			
+			System.out.println("Les lletres del array són:  "+ nom[i]);
+		}
 	}	
 			
 }
