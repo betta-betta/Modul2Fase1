1c22c0b772d8538b045a3e8e440448a6fdc24d01
Lluis Gutierrez <betta_betta@hotmail.com>
2021-03-06 16:03:20 +0100

    Modul2Fase1ok

--- src/Modul2Fase1.java
+++ src/Modul2Fase1.java
@@ -1,40 +1,19 @@
+import java.util.ArrayList;
 
 public class Modul2Fase1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-int num1=4;
-int num2=0;
-
-boolean resultado=suma(num1,num2);
-
-
-if(resultado) {
-	System.out.println( resultado);
-}
-else {
-	System.out.println(resultado);
-}
-/*if (suma(num1,num2)) {
-	System.out.println(" valor es mayor de 0 o ");
-}
-else {
-	System.out.println("El valor es menor que 0 o ");
-}*/
-
-//System.out.println("el resutado de la suma es: "+resultado);
-
-	}
-public static boolean suma(int num1,int num2) {
-	int resultado=num1+num2;
-	if(resultado>0) {
-		return true;
+public static void main(String[] args) {
 		
-	}
-	else {
-		return false;
-	}
-	
-	}
-}
 
+		ArrayList<Character> nom = new ArrayList <Character>();
+		nom.add('L');
+		nom.add('l');
+		nom.add('u');
+		nom.add('i');
+		nom.add('s');
+		
+		System.out.println("Total de caràcters al ArrayList son : "+nom.size());
+		System.out.println(nom);
+	}	
+			
+}
